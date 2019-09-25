ae9cf3a2ad377b251bbed2da626a9a01204e1b6c
April Li <aprilli@Aprils-MacBook-Pro.local>
2019-09-25 14:08:08 -0400

    Worked on up to problem 5

--- Recursion.java
+++ Recursion.java
@@ -5,12 +5,23 @@
 *
 */
 import structure5.*;
+import java.util.Arrays;
 
 public class Recursion {
 
   /***** Warmup 0.2 ********************************************/
   public static boolean canMakeSum(int setOfNums[], int targetSum) {
-    return false;
+    if(setOfNums.length == 0){
+      return false;
+    }
+    else{
+      for(int j = 1; j < setOfNums.length; j++){
+        if(setOfNums[0]+setOfNums[j] == targetSum || setOfNums[0] == targetSum || setOfNums[j] == targetSum){
+          return true;
+        }
+      }
+      return canMakeSum(Arrays.copyOfRange(setOfNums,1,setOfNums.length), targetSum);
+    }
   }
 
   /*****  1  ***************************************************/
@@ -22,7 +33,14 @@ public class Recursion {
   * Big-O runtime:
   */
   public static int countCannonballs(int height) {
-    return 0;
+    int sum = 0;
+    if(height == 1){
+      sum += 1;
+    }
+    else{
+      sum += (height*height)+countCannonballs(height-1);
+    }
+    return sum;
   }
 
 
@@ -34,8 +52,17 @@ public class Recursion {
   * post:
   * Big-O runtime:
   */
+//empty string counts as palindrome right?
   public static boolean isPalindrome(String str) {
-    return false;
+    if(str.length() == 0 || str.length() == 1){
+      return true;
+    }
+    else if(str.substring(0,1).equals(str.substring(str.length()-1))){
+      return isPalindrome(str.substring(1,str.length()-1));
+    }
+    else{
+      return false;
+    }
   }
 
   /*****  3  ***************************************************/
@@ -49,7 +76,17 @@ public class Recursion {
   * Big-O runtime:
   */
   public static boolean isBalanced(String str) {
-    return false;
+    if(str.length() == 0){
+      return true;
+    }
+    else{
+      for(int i = 0; i < str.length()-1; i++){
+        if(str.substring(i,i+2).equals("()") || str.substring(i,i+2).equals("[]") || str.substring(i,i+2).equals("{}")){
+          return isBalanced(str.substring(0,i)+str.substring(i+2));
+        }
+      }
+      return false;
+    }
   }
 
   /*****  4  ***************************************************/
@@ -62,7 +99,23 @@ public class Recursion {
   * Big-O runtime:
   */
   public static void substrings(String str) {
-    System.out.println(str);
+    if(str.length() == 0){
+      System.out.print("\"\"");
+    }
+    else{
+      System.out.print("\"" + str.charAt(0) + "\", ");
+      if(str.length() > 1){
+        for(int i = 1; i < str.length(); i++){
+          System.out.print("\"" + str.charAt(0) + str.charAt(i) + "\", ");
+        }
+      }
+      if(str.length() > 2){
+        for(int i = 2; i < str.length(); i++){
+          System.out.print("\"" + str.substring(0,i+1) + "\", ");
+        }
+      }
+      substrings(str.substring(1));
+    }
   }
 
   /*
@@ -74,6 +127,7 @@ public class Recursion {
   * post:
   * Big-O runtime:
   */
+//do we have to use the helper method?
   public static void substringHelper(String str, String soFar) {
 
   }
@@ -88,7 +142,23 @@ public class Recursion {
   * Big-O runtime:
   */
   public static void printInBinary(int number) {
-    System.out.println(0);
+    int mult = 1;
+    while(mult < (number/2)+1){
+      mult = mult*2;
+    }
+    if(mult == 1){
+      System.out.print(number%2);
+    }
+    else{
+      if(mult <= number){
+        System.out.print(1);
+        printInBinary(mult/2);
+      }
+      else{
+        System.out.print(0);
+      }
+      //printInBinary(number-((mult/2)+mult));
+    }
   }
 
 
@@ -144,17 +214,22 @@ public class Recursion {
 
     // Please add your own tests to supplement these
     // Not all cases are thoroughly checked!!!
-
+/*
+    System.out.println(countCannonballs(7));
     System.out.println(countCannonballs(3));
     System.out.println(countCannonballs(10));
 
     System.out.println(isPalindrome("mom"));
+    System.out.println(isPalindrome(""));
+    System.out.println(isPalindrome("youth"));
     System.out.println(isPalindrome("deeded"));
+    System.out.println(isPalindrome("level"));
     System.out.println(isPalindrome("ablewasIereIsawelba"));
 
-    System.out.println(isBalanced("[{[()()]}]"));
-    System.out.println(isBalanced("[{[()()]}][{[()()]}]"));
-    System.out.println(isBalanced("[{[()()]}{]{[()()]}]"));
+    System.out.println(isBalanced("[{[()()]}]")); //true
+    System.out.println(isBalanced("[{[(){(})]}]")); //false
+    System.out.println(isBalanced("[{[()()]}][{[()()]}]")); //true
+    System.out.println(isBalanced("[{[()()]}{]{[()()]}]")); //false
 
     substrings("abc");
     System.out.println();
@@ -164,8 +239,20 @@ public class Recursion {
     System.out.println();
     substrings("");
     System.out.println();
-
-    printInBinary(0);
+*/
+    printInBinary(1);
+    System.out.println();
+    printInBinary(2);
+    System.out.println();
+    printInBinary(3);
+    System.out.println();
+    printInBinary(4);
+    System.out.println();
+    printInBinary(5);
+    System.out.println();
+    printInBinary(6);
+    System.out.println();
+/*    printInBinary(0);
     System.out.println();
     printInBinary(30);
     System.out.println();
@@ -179,11 +266,14 @@ public class Recursion {
     System.out.println();
 
     int[] numSet = {2, 5, 7, 12, 16, 21, 30};
+    System.out.println(canMakeSum(numSet, -1));
+    System.out.println(canMakeSum(numSet, 19));
+    System.out.println(canMakeSum(numSet, 100));
     System.out.println(canMakeSum(numSet, 21));
     System.out.println(canMakeSum(numSet, 22));
     System.out.println(canMakeSum(numSet, 3));
     System.out.println(canMakeSum(numSet, 30));
-
+/*
     System.out.println(printSubSetSum(numSet, 21));
     System.out.println(printSubSetSum(numSet, 22));
     System.out.println(printSubSetSum(numSet, 3));
@@ -193,6 +283,6 @@ public class Recursion {
     System.out.println(countSubSetSumSolutions(numSet, 22));
     System.out.println(countSubSetSumSolutions(numSet, 3));
     System.out.println(countSubSetSumSolutions(numSet, 30));
-
+*/
   }
 }
